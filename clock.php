3fad3f40352388ee524eea863f8538193149ca51
Shuai Qi <qishuai231@gmail.com>
2017-01-24 15:37:55 +0800

    adjust hour display（degree）

--- clock.php
+++ clock.php
@@ -15,7 +15,7 @@ imagearc($img, 150, 150, 148, 148, 0, 0, $black);
 //时钟中心
 imagefilledarc($img, 150, 150, 4, 4, 0, 0, $black, IMG_ARC_PIE);
 
-//画一周刻度线
+//刻度线
 for ($i=0;$i<60;$i++){
     if ($i % 5 == 0){
         imageline($img, 150-65*cos(deg2rad(6*$i)), 150 + 65*sin(deg2rad(6*$i)), 150-75*cos(deg2rad(6*$i)), 150 + 75*sin(deg2rad(6*$i)), $black);
@@ -30,11 +30,12 @@ $date = explode(' ', $date);
 $hour = $date[0] * 30;
 $minute = $date[1] * 6;
 $second = $date[2] * 6;
-imageline($img, 150, 150, 150+30*sin(deg2rad($hour+30/($minute*6))), 150-30*cos(deg2rad($hour+30/($minute*6))), $red);
+$radio = $minute==0?1:60/$date[1];
+imageline($img, 150, 150, 150+30*sin(deg2rad($hour+30/$radio)), 150-30*cos(deg2rad($hour+30/$radio)), $red);
 imageline($img, 150, 150, 150+40*sin(deg2rad($minute)), 150-40*cos(deg2rad($minute)), $red);
 imageline($img, 150, 150, 150+55*sin(deg2rad($second)), 150-55*cos(deg2rad($second)), $red);
 
-
+//图像输出
 header("Content-type:image/png");
 imagepng($img);
 imagedestroy($img);
